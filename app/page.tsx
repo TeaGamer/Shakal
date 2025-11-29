6afcad6a2e7429ae9344e82af13e93fcedd22464
Renat <rentabbasov@gmail.com>
2025-11-29 22:04:04 +0100

    0.3

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,26 @@
 // app/layout.tsx
 import "./global.css";
 
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="uk">
       <body>
         <div className="app-bg">
+        <header>
+          <img
+           src="/FastMoney.png"          // твій файл
+           alt="site logo"
+           className="top-logo"
+          />
+          </header>
           <main className="center-wrapper">
             <div className="center-card">
-              {/* Тут з'явиться текст/відео/аудіо — наразі плейсхолдер */}
-              {children ?? (
-                <div className="center-placeholder">
-                  <h2>Все буде тут</h2>
-                </div>
-              )}
+              {children}<p>Все пише тут</p>
+              
             </div>
           </main>
+
         </div>
       </body>
     </html>
